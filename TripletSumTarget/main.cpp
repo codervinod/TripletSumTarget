17cb647cc5f3e3e4f78948011cc803e46c3ca661
Vinod Gupta <vinod_gupta@live.com>
2015-01-28 15:33:56 -0800

    initial check in

--- TripletSumTarget/main.cpp
+++ TripletSumTarget/main.cpp
@@ -6,10 +6,55 @@
 //  Copyright (c) 2015 Vinod Gupta. All rights reserved.
 //
 
+#include <vector>
 #include <iostream>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void printArray(int ar[],int high)
+{
+    for (int i = 0; i < high; ++i)
+    {
+        std::cout << ar[i] << "\t";
+    }
+    std::cout << std::endl;
+}
+
+bool findTripletWithSum(int ar[], int high, int sum)
+{
+    for (int i = 0; i < high; ++i)
+    {
+        int itr = i+1;
+        int revItr = high - 1;
+        while (itr < revItr)
+        {
+            int tmp = ar[i] + ar[itr] + ar[revItr];
+            if (tmp < sum)
+            {
+                ++itr;
+            }
+            else if (tmp > sum)
+            {
+                --revItr;
+            }
+            else
+            {
+                std::cout << ar[i] << "\t" << ar[itr] << "\t" << ar[revItr] << "\n";
+                return true;
+            }
+        }
+    }
+    std::cout << "Not found" << std::endl;
+    return false;
+}
+
+int main(int argc, char* argv[])
+{
+    int ar[] = { 3, 9, 2, 7, 34, 12, 42, 22, 52 };
+    int high = sizeof ar / sizeof(int);
+    
+    printArray(ar, high);
+    std::sort(std::begin(ar), std::end(ar));
+    findTripletWithSum(ar, high, 51);
     return 0;
+    
 }
